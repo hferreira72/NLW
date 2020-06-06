8eccbf677d8ed4043c9aaff7f0da0f5b23d1e2aa
hferreira72 <39095895+hferreira72@users.noreply.github.com>
2020-06-06 12:53:32 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,103 @@
+const express = require("express")
+const server = express()
+
+const db = require("./database/db.js")
+
+// configurar pasta public
+server.use(express.static("public"))
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true}))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+// página inicial
+// req -> requisição
+// res -> resposta
+
+server.get("/", (req, res) => {
+   return res.render("index.html", { title: "Bagaaaaaça"} )
+})
+
+server.get("/create-point", (req, res) => {
+   return res.render("create-point.html")
+
+   // req.query: query strings da nossa URL
+   // console.log(req.query)
+})
+
+server.post("/savepoint", (req, res) => {
+
+   // req.body: o corpo do nosso formulário
+   // console.log(req.body)
+
+   // inserir dados no banco de dados
+   const query = `
+   INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+   ) VALUES (?,?,?,?,?,?,?)
+`
+const values = [
+   req.body.image,
+   req.body.name,
+   req.body.address,
+   req.body.address2,
+   req.body.state,
+   req.body.city,
+   req.body.items
+]
+
+function afterInsertData(err) {
+   if(err) {
+      console.log(err)
+      return res.send("Erro no Cadastro!")
+   }
+   console.log("Cadastrado com sucesso")
+   console.log(this)
+   
+   return res.render("create-point.html", { saved: true })
+}
+
+ db.run(query, values, afterInsertData)
+
+})
+
+server.get("/search-results", (req, res) => {
+
+   const search = req.query.search
+
+   if(search == "") {
+      //pesquisa vazia
+      return res.render("search-results.html", { total: 0})
+
+   }
+   db.all(`select * from places where city like '%${search}%'`, function(err, rows) {
+      if(err) {
+          return console.log(err)
+      }
+
+      console.log("Aqui estão seus registros: ")
+      console.log(rows)
+
+      const total = rows.length
+
+      // mostrar a pagina html com os dados do banco
+      return res.render("search-results.html",{places: rows, total: total})
+  })
+
+})
+
+// ligar o servidor
+server.listen(3000)
